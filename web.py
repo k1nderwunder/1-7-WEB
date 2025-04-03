fd98230d75c9df203d343c8eee75bb5b03867b58
k1nderwunder <maxday202020@gmail.com>
2025-04-03 15:57:36 +0700

    Задание 2

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,3 +9,12 @@ def read_root():
     with open("index.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+@app.post("/calculate")
+def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {"result": result}
